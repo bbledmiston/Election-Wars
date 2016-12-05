90d8b28d9427cf6ddaabda35e2fede05bb46c3c6
Brittney Edmiston <bedmiston@Brittneys-MacBook-Air.local>
2016-12-05 13:36:57 -0500

    changes1

--- electionwars.py
+++ electionwars.py
@@ -19,7 +19,33 @@ controller - campaign
 main
 - call campaign
 c.run campaign - loops throughg
+
+json file to create login data
+active plot graph to update polling data
 """
+
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.rating = None
+
+    #def create user data?
+
+    def rating(self):
+        polling = 0
+        attempt, outcomeA, outcomeB = ask_question(questions)
+        if attempt == outcomeA:
+            polling+=outcomeA
+        elif attempt == outcomeB:
+            polling+=outcomeB
+        else:
+            print("Error")
+        self.rating = polling
+        return self.rating
+
+    def __str__(self):
+        return self.name + ", Your rating is: " + str(self.rating)
+
 
 class Controller:
     def __init__(self):
@@ -27,38 +53,45 @@ class Controller:
 
 
 
-class Player:
-    def __init__(self,name,rating):
-        self.name = name
-        self.rating = rating
-
-
 
 class Events:
     def __init__(self):
-        self.july = {
-                'You have emerged the champion of your party! Are you respectful in your acceptance of the nomination?':
+        self.months = {}
+        self.months['july'] = {
+                'You have emerged the champion of your party!':
                 ('\na. Yes\nb. No\n', 1,-1),
                 'Test question':
                 ('\na. +1\nb. -1\n', 1,-1)
                 }
         self.august = {
+                'You accuse your opponent of wanting to abolish guns. Do you make a bad joke that can be interpreted as a call to violence?':
+                ('\na. Yes \nb. No\n', -1,1)
 
         }
 
         self.september = {
-        'appeal to minority voters?':('\na. Yes\nb. No\n', 1,-1)
+                'assuage the fears of minority voters?':
+                ('\na. Nah\nb. I don\'\t think so \n', -1,-1),
+                'you\'\re attending a congregation at a black church. you are asked to not give a political speech. do you...':
+                ('\n a. comply\nb. give the speech anyway',1,-1)
+                'You again accuse your opponent of wanting to abolish the second amendment. Do you make another incendiary joke?':
+                ('\na. No \nb. Hell yeah\n', 1,-1)
 
         }
 
         self.october = {
-
+                'test question':
+                ('\na. Yes\nb. No\n', 1,-1)
         }
 
         self.november = {
-        'would you vote for you?':('\na. Yes\nb. No\n',0,0)
+                'Do you feel you are completely qualified to be the president of the United States of America?':
+                ('\na. Yes\nb. No\n',0,0),
+                'would you vote for you?':
+                ('\na. Yes\nb. No\n',0,0)
         }
-    def ask_question(questions):
+
+    def ask_question(self,questions):
         item = random.choice(list(questions.items()))
         question = item[0]
         print(question)
@@ -67,25 +100,19 @@ class Events:
         attempt = input('\nEnter \'a\' or \'b\' for your answer\n')
         return (attempt, outcomeA, outcomeB)
 
-    def rating():
-        polling = 0
-        attempt, outcomeA, outcomeB = ask_question(questions)
-        if attempt == outcomeA:
-            polling+=outcomeA
-        elif attempt == outcomeB:
-            polling+=outcomeB
-        else:
-            print("invalid")
+
 
 def main():
-    numQuestions = len(list(questions.keys()))
-    for i in range(numQuestions):
-        attempt, outcomeA, outcomeB = ask_question(questions)
-        if attempt not in {'a', 'b'}:
-            print('invalid input')
-        elif attempt == 'a':
-            print(outcomeA)
-        elif attempt == 'b':
-            print(outcomeB)
+    p = Player("bob")
+    print(p)
+    # numQuestions = len(list(questions.keys()))
+    # for i in range(numQuestions):
+    #     attempt, outcomeA, outcomeB = ask_question()
+    #     if attempt not in {'a', 'b'}:
+    #         print('invalid input')
+    #     elif attempt == 'a':
+    #         print(outcomeA)
+    #     elif attempt == 'b':
+    #         print(outcomeB)
 
 main()
