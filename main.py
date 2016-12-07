ccf48167a49878874f790533d290cde64833c91e
Brittney Edmiston <bedmiston@Brittneys-MacBook-Air.local>
2016-12-07 18:59:20 -0500

    cleaning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import tkinter
-# import "tkinter" messagebox
-# import twoelectionwars
 import twoelectionwars
 
 class Controller:
@@ -8,32 +6,19 @@ class Controller:
     def __init__(self):
         self.__mainWindow = tkinter.Tk()
         self.__mainWindow.title ('Controller')
-        # self.top_frame = tkinter.Frame(self.__mainWindow)
-        # self.bottom_frame = tkinter.Frame(self.__mainWindow)
         self.top_frame = tkinter.Frame(self.__mainWindow)
         self.bottom_frame = tkinter.Frame(self.__mainWindow)
-        # self.question = tkinter.StringVar()
         self.questionList = ["a","b","c","d"]
-        self.question = self.questionList[0]
-        self.q1 = self.questionList[0]
-        # self.question.set(self.q1)
+        self.choiceList = ["Choice A or Choice B","Choice C or Choice D","Choice A or Choice B","Choice C or Choice D"]
         self.buttonpressed = False
         self.index = 0
         for i in range(len(self.questionList)):
-        # for i in range(len(self.questionList)):
-
-
-            # i = 0
-            # self.index += 1
-            # if self.index < len(self.questionList):
-            #     print(self.question)
             self.question = self.questionList[self.index]
-
-            # self.question.set(questionList[i])
+            self.choice = self.choiceList[self.index]
             self.__test = twoelectionwars.twoelectionwars(0,self.question,"a. Nah","b. I don't think so",1,-1)
             # print(self.__test.rating1)
             self.__label = tkinter.Label(self.top_frame, text=self.question)
-            self.__label2 = tkinter.Label(self.bottom_frame, text="I am also a label")
+            self.__label2 = tkinter.Label(self.bottom_frame, text=self.choice)
 
             self.__button1 = tkinter.Button(self.__mainWindow, \
                                                 text="A", \
@@ -43,9 +28,6 @@ class Controller:
                                                 text="B", \
                                                 command=self.ratingcalc
                                                 )
-            # print(i)
-            # if self.ratingcalc() == True:
-            #     i += 1
 
 
         self.top_frame.pack() #frames have to be packed first
@@ -54,20 +36,11 @@ class Controller:
         self.__label2.pack()
         self.__button1.pack(side="left")
         self.__button2.pack(side="right")
-            # if self.index == len(self.questionList)-1:
-            #     self.buttonpressed = True
-            # i += 1
-            # self.buttonpressed = True
-        print(self.index)
-        print(self.buttonpressed)
-        # self.__label = tkinter.Label(self.top_frame, text=twoelectionwars.questions())
-            #self.index += 1
-
 
 
         #packing automatically stacks based on order
-        self.top_frame.pack() #frames have to be packed first
-        self.bottom_frame.pack()
+        # self.top_frame.pack() #frames have to be packed first
+        # self.bottom_frame.pack()
         # # self.__label.pack()
         # self.__label2.pack()
         # self.__button1.pack(side="left")
@@ -82,12 +55,12 @@ class Controller:
         self.index = self.index % 4
         self.buttonpressed = True
         self.__label.config(text=self.questionList[self.index])
+        self.__label2.config(text=self.choiceList[self.index])
         print(self.index)
         print(self.buttonpressed)
 
         # return self.index
 
-#string vars - tkinter
 def main():
     c = Controller()
     print(c.ratingcalc())
